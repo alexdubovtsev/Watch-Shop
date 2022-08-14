ebc240748955f75e29f1baa2c69a2a478c0e4490
ALEXANDER DUBOVTSEV <alexdubovtsev.it@gmail.com>
2022-08-14 20:54:54 +0200

    Add Header body

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,40 @@
-import * as Functions from "./modules/functions.js"; 
-Functions.isWebp();
+// todo Main Modules
+
+import * as functions from "./modules/functions.js"; 
+
+functions.isWebp();
+
+// functions.addTouchClass();
+
+// functions.addLoadedClass();
+
+// functions.menuInit();  
+
+// functions.spollers();
+
+// functions.showMore();
+
+// functions.tabs();
+
+// functions.initPopups();
+
+
+// todo Forms
+
+// import * as forms from "./modules/forms.js"; 
+
+// forms.formFieldsInit(); // Add classes, placeholder
+
+// forms.formSubmit(true); // Validation (false - off)
+
+// forms.formMasks(); 
+
+// forms.formViewPassword(); 
+
+// forms.formRating();
+
+// forms.formSelect();
+
 
 import Swiper, { Navigation, Pagination } from 'swiper';
 
