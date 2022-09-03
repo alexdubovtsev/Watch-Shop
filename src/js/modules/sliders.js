4dbe0330d891fbd61120c5b6e14a578931b99400
ALEXANDER DUBOVTSEV <alexdubovtsev.it@gmail.com>
2022-09-03 15:08:38 +0200

    Fix Product card slider

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -100,7 +100,119 @@ function initSliders() {
 				}
 			}
 		});
-	}
+	};
+
+	// if (document.querySelector('.products-slider__slider')) {
+	// 	new Swiper('.products-slider__slider', {
+	// 		// Подключаем модули слайдера
+	// 		// для конкретного случая
+	// 		modules: [Navigation, Pagination, Autoplay],
+
+	// 		// effect: 'fade',
+
+	// 		autoplay: {
+	// 			delay: 3500,
+	// 			disableOnInteraction: false,
+	// 		},
+			
+	// 		observer: true,
+	// 		observeParents: true,
+	// 		slidesPerView: 3,
+	// 		spaceBetween: 30,
+	// 		autoHeight: true,
+	// 		speed: 800,
+	// 		//touchRatio: 0,
+	// 		//simulateTouch: false,
+	// 		loop: true,
+	// 		//preloadImages: false,
+	// 		//lazy: true,
+	// 		// Dotts
+	// 		pagination: {
+	// 			el: '.products-slider__dotts',
+	// 			clickable: true,
+	// 		},
+			
+	// 		/*
+	// 		breakpoints: {
+	// 			320: {
+	// 				slidesPerView: 1,
+	// 				spaceBetween: 0,
+	// 				autoHeight: true,
+	// 			},
+	// 			768: {
+	// 				slidesPerView: 2,
+	// 				spaceBetween: 20,
+	// 			},
+	// 			992: {
+	// 				slidesPerView: 3,
+	// 				spaceBetween: 20,
+	// 			},
+	// 			1268: {
+	// 				slidesPerView: 4,
+	// 				spaceBetween: 30,
+	// 			},
+	// 		},
+	// 		*/
+	// 		on: {
+	// 		}
+	// 	});
+	// }
+
+	if (document.querySelector('.products-slider__slider')) {
+		new Swiper('.products-slider__slider', {
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Navigation, Pagination, Parallax],
+
+			// effect: 'fade',
+	
+			observer: true,
+			observeParents: true,
+			watchOverflow: true,
+			slidesPerView: 4,
+			spaceBetween: 40,
+			autoHeight: true,
+			speed: 800,
+			parallax: true,
+			//touchRatio: 0,
+			//simulateTouch: false,
+			//loop: true,
+			//preloadImages: false,
+			//lazy: true,
+			// Dotts
+			pagination: {
+				el: '.products-slider__dotts',
+				clickable: true,
+			},
+			
+			/*
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
+			on: {
+				init: function(swiper) {
+					
+				}
+			}
+		});
+	};
 }
 // Скролл на базе слайдера (по классу swiper_scroll для оболочки слайдера)
 function initSlidersScroll() {
